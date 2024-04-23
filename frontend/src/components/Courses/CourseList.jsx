bcce8fc74c0420638ffe4e62be46e4c1b32ba65f
Sangya Sherpa <135448616+13x54n@users.noreply.github.com>
2024-04-23 23:33:13 +0000

    Added Mock Courses

--- frontend/src/components/Courses/CourseList.jsx
+++ frontend/src/components/Courses/CourseList.jsx
@@ -3,63 +3,62 @@ const products = [
       id: 1,
       name: 'dAppp Development',
       href: '#',
-      imageSrc: 'https://images.unsplash.com/photo-1526378800651-c32d170fe6f8?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      imageAlt: "Front of men's Basic Tee in black.",
+      imageSrc: 'https://images.unsplash.com/photo-1559445368-b8a993676d7a?q=80&w=3131&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      imageAlt: "",
       reviews: '8.9/10',
       updatedAt: 'August 21, 2023',
       description: "Learn how to build a simple dApp on EVM Compatible Chains."
     },
     {
       id: 2,
-      name: 'dAppp Development',
+      name: 'Online Marketing',
       href: '#',
-      imageSrc: 'https://images.unsplash.com/photo-1526378800651-c32d170fe6f8?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      imageAlt: "Front of men's Basic Tee in black.",
-      reviews: '8.9/10',
-      updatedAt: 'August 21, 2023',
-      description: "Learn how to build a simple dApp on EVM Compatible Chains."
+      imageSrc: 'https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      imageAlt: "",
+      reviews: '7.5/10',
+      updatedAt: 'May 26, 2023',
+      description: "Online marketing has become the core functionalities for modern business."
     },
     {
       id: 3,
-      name: 'dAppp Development',
+      name: 'Optimize Web Performance',
       href: '#',
-      imageSrc: 'https://images.unsplash.com/photo-1526378800651-c32d170fe6f8?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      imageAlt: "Front of men's Basic Tee in black.",
-      reviews: '8.9/10',
-      updatedAt: 'August 21, 2023',
-      description: "Learn how to build a simple dApp on EVM Compatible Chains."
+      imageSrc: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      imageAlt: "",
+      reviews: '6/10',
+      updatedAt: 'July 26, 2023',
+      description: "This course os designed for those new to web performance, a vital aspect."
     },
     {
       id: 4,
-      name: 'dAppp Development',
+      name: 'Tax Filing & Account in Ontario',
       href: '#',
-      imageSrc: 'https://images.unsplash.com/photo-1526378800651-c32d170fe6f8?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      imageAlt: "Front of men's Basic Tee in black.",
-      reviews: '8.9/10',
-      updatedAt: 'August 21, 2023',
-      description: "Learn how to build a simple dApp on EVM Compatible Chains."
+      imageSrc: 'https://images.unsplash.com/photo-1554224154-26032ffc0d07?q=80&w=3226&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      imageAlt: "",
+      reviews: '5.4/10',
+      updatedAt: 'April 02, 2024',
+      description: "Tax filing and reporting made for individuals and know about every benefits you get."
     },
     {
       id: 5,
-      name: 'dAppp Development',
+      name: 'Home Studio Production',
       href: '#',
-      imageSrc: 'https://images.unsplash.com/photo-1526378800651-c32d170fe6f8?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      imageAlt: "Front of men's Basic Tee in black.",
-      reviews: '8.9/10',
-      updatedAt: 'August 21, 2023',
-      description: "Learn how to build a simple dApp on EVM Compatible Chains."
+      imageSrc: 'https://images.unsplash.com/photo-1511379938547-c1f69419868d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      imageAlt: "",
+      reviews: '9.6/10',
+      updatedAt: 'October 10, 2023',
+      description: "Grammy level musics can be produced from your home to billions of streaming platforms."
     },
     {
       id: 6,
-      name: 'dAppp Development',
+      name: 'Interior Design 101',
       href: '#',
-      imageSrc: 'https://images.unsplash.com/photo-1526378800651-c32d170fe6f8?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      imageAlt: "Front of men's Basic Tee in black.",
-      reviews: '8.9/10',
-      updatedAt: 'August 21, 2023',
-      description: "Learn how to build a simple dApp on EVM Compatible Chains."
+      imageSrc: 'https://images.unsplash.com/photo-1534349762230-e0cadf78f5da?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      imageAlt: "",
+      reviews: '10/10',
+      updatedAt: 'July 1, 2023',
+      description: "ABC of interior designs, tools, and framing the interiors minimally."
     },
-    // More products...
   ]
   
   export default function CourseLists() {
