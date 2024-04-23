f1a2cea41ae4f735d44e1e0d632a90db5f8239d6
13x54n <13x54r@gmail.com>
2024-04-23 17:53:38 -0400

    Added Courses Lists

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import CoursesFilters from "../components/Courses/CoursesFilters";
 
 export default function Courses() {
   return (
-    <div>Courses</div>
-  )
+    <div className="courses__container">
+      <img
+        src="https://developers.google.com/learn/images/learn-header.svg"
+        alt=""
+        className="w-[100vw]"
+      />
+
+      <div className="mx-[5vw] my-3">
+        <h1 className="mt-2 text-3xl font-medium tracking-tight text-gray-900 sm:text-4xl">
+          A better workflow
+        </h1>
+        <p className="mt-2 text-lg leading-8 text-gray-700">
+          Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At
+          arcu, sit dui mi.
+        </p>
+      </div>
+
+      <div>
+        <CoursesFilters />
+      </div>
+    </div>
+  );
 }
