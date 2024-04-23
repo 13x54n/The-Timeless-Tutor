70a278461878050d680f206f70aa5123e61fa4a1
Sangya Sherpa <135448616+13x54n@users.noreply.github.com>
2024-04-23 22:38:20 +0000

    Added Blank Pages

--- frontend/src/components/Courses/CourseList.jsx
+++ frontend/src/components/Courses/CourseList.jsx
@@ -59,7 +59,7 @@ const products = [
   export default function CourseLists() {
     return (
       <div className="bg-white">
-        <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mx-auto">
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {products.map((product) => (
               <div key={product.id} className="group relative">
