12fb28b0d7ce9e4ee1c830d5dff0c52d3b096a13
Sangya Sherpa <135448616+13x54n@users.noreply.github.com>
2024-04-23 23:41:43 +0000

    Working on mobile notifications

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -325,12 +325,131 @@ export default function Navbar() {
                 </a>
               </div>
               <div className="py-6">
-                <a
-                  onClick={() => handleNavigation("/auth/login")}
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
+                {user ? (
+                  <>
+                    (
+                    <div className="flex -space-x-1">
+                      <Menu
+                        as="div"
+                        className="relative inline-block text-left"
+                      >
+                        <div>
+                          <Menu.Button className="inline-flex w-full items-center justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-50">
+                            {console.log(user.photoURL)}
+                            {user.photoURL !== null ? (
+                              <img
+                                className="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+                                src={user.photoURL}
+                                alt={user.email}
+                              />
+                            ) : (
+                              <span className="text-lg bg-[#dfe4ea] w-8 h-8 flex items-center justify-center rounded-full">
+                                {user.email[0]}
+                              </span>
+                            )}
+                            {user.email.split("@")[0]}
+                            <ChevronDownIcon
+                              className="-mr-1 h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                          </Menu.Button>
+                        </div>
+
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className={classNames(
+                                      active
+                                        ? "bg-gray-100 text-gray-900"
+                                        : "text-gray-700",
+                                      "block px-4 py-2 text-sm"
+                                    )}
+                                  >
+                                    Account settings
+                                  </a>
+                                )}
+                              </Menu.Item>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className={classNames(
+                                      active
+                                        ? "bg-gray-100 text-gray-900"
+                                        : "text-gray-700",
+                                      "block px-4 py-2 text-sm"
+                                    )}
+                                  >
+                                    Support
+                                  </a>
+                                )}
+                              </Menu.Item>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className={classNames(
+                                      active
+                                        ? "bg-gray-100 text-gray-900"
+                                        : "text-gray-700",
+                                      "block px-4 py-2 text-sm"
+                                    )}
+                                  >
+                                    License
+                                  </a>
+                                )}
+                              </Menu.Item>
+                              <form
+                                onSubmit={() =>
+                                  auth
+                                    .signOut()
+                                    .then(() => navigate("/"))
+                                    .catch((err) => console.log(err))
+                                }
+                              >
+                                <Menu.Item>
+                                  {({ active }) => (
+                                    <button
+                                      type="submit"
+                                      className={classNames(
+                                        active
+                                          ? "bg-gray-100 text-gray-900"
+                                          : "text-gray-700",
+                                        "block w-full px-4 py-2 text-left text-sm"
+                                      )}
+                                    >
+                                      Sign out
+                                    </button>
+                                  )}
+                                </Menu.Item>
+                              </form>
+                            </div>
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+                    </div>
+                    )
+                  </>
+                ) : (
+                  <a
+                    onClick={() => handleNavigation("/auth/login")}
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white-900 hover:bg-gray-50"
+                  >
+                    Log in
+                  </a>
+                )}
               </div>
             </div>
           </div>
